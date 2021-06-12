3501c03d275fb2b6876efffc349a253bd1d6da0b
lcarr99 <carrdrfc99@gmail.com>
2021-06-12 16:58:07 +0100

    inital upload

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>Laracasts Voting</title>
+
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
+    </head>
+    <body class="font-sans bg-gray-background text-gray-900 text-sm">
+        <header class="flex items center justify-between px-8 py-4">
+            <a href="#"><img src="{{ asset('logo.svg') }}" alt="Laracasts Logo"></a>
+            <div class="flex items-center">
+            @if (Route::has('login'))
+                <div class="px-6 py-4">
+                    @auth
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+
+                            <a href="route('logout')"
+                                    onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                                {{ __('Log Out') }}
+                            </a>
+                        </form>
+                    @else
+                        <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+                <a href="#">
+                    <img src="https://www.gravatar.com/avatar/000000000000000000000000000?d=mp" alt="avatar" class="w-10 h-10 rounded-full">
+                </a>
+            </div>
+        </header>
+        
+        <main class="container mx-auto max-w-custom flex">
+            <div class="w-70 mr-5">
+                Add Idea from goes here.
+            </div>
+            <div class="w-175">
+                <nav class="flex items-center justify-between text-xs">
+                    <ul class="flex uppercase font-semibold border-b-4 pb-3 space-x-10">
+                        <li><a href="#" class="border-b-4 pb-3 border-blue">All Ideas (87)</a></li>
+                        <li><a href="#"></a></li>
+                        <li></li>
+                    </ul>
+                </nav>
+            </div>
+        </main>
+    </body>
+</html>
